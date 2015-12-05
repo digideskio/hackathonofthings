131bd8c9850269dcc4be4acc2e2eb02d47a3ed66
Sami Suo-Heikki <suoheikki.sami@gmail.com>
2015-12-05 17:28:30 +0200

    Layout mixed with data

--- index.php
+++ index.php
@@ -1,11 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Harness racing</title>
-    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="C:/xampp/favicon.ico">
+
+    <title>Harness Racing Wager</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
+
 <body>
+
+
 <?php
 include 'passwords.php';
 include 'curl.php';
@@ -45,7 +79,7 @@ foreach ($horses as $horse) {
     $horseRow->luminance = countPercent($previousRaces, $currentLuminance, 'luminance');
     $horseRow->pressure = countPercent($previousRaces, $currentPressure, 'pressure');
     $horseRow->temperature = countPercent($previousRaces, $currentTemperature, 'temperature');
-    $horseRow->totalSuccess = ($horseRow->humidity + $horseRow->luminance + $horseRow->pressure + $horseRow->temperature) / 4;
+    $horseRow->totalWinning = ($horseRow->humidity + $horseRow->luminance + $horseRow->pressure + $horseRow->temperature) / 4;
     array_push($horseRows, $horseRow);
 }
 
@@ -68,6 +102,123 @@ function countPercent($previousRaces, $currenctHumidity, $field) {
     return round((1 - ($humidityPosition / 6)) * 100, 2);
 }
 ?>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="#">
+                <i class="fa fa-trophy"></i> <span class="light">Harness Raging Wager</span>
+            </a>
+        </div>
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+            <ul class="nav navbar-nav">
+                <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#about">Wages</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#download">Profile</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#contact">Settings</a>
+                </li>
+            </ul>
+        </div><!--/.navbar-collapse -->
+    </div>
+</nav>
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+    <div class="container">
+        <h1>Harness Racing Wager!</h1>
+        <p>This site is for waging in harness races</p>
+
+        <div class="row">
+            <div class="col-md-3">
+                <i class="fa fa-sun-o"></i> <?php echo $currentLuminance; ?> luminance
+            </div>
+            <div class="col-md-3">
+                <i class="fa fa-fire"></i> <?php echo $currentTemperature; ?> temperature
+            </div>
+            <div class="col-md-3">
+                <i class="fa fa-umbrella"></i> <?php echo $currentHumidity; ?> humidity
+            </div>
+            <div class="col-md-3">
+                <i class="fa fa-wrench"></i> <?php echo $currentPressure; ?> pressure
+            </div>
+        </div>
+        <!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+		-->
+    </div>
+</div>
+
+
+<div class="container">
+    <!-- Example row of columns -->
+    <div class="row">
+        <div class="col-md-3">
+            <div class="btn-group" data-toggle="buttons">
+                <?php
+                foreach($database->getRaces() as $race) {
+                    echo '<label class="btn btn-default btn-lg btn-block active">
+                            <input type="checkbox" autocomplete="off" checked>'.$race->date.'
+                          </label>';
+                }
+                ?>
+            </div>
+        </div>
+
+        <div class="col-md-9">
+
+            <table class="table table-bordered">
+                <!-- TABLE HEAD -->
+                <thead style="font-weight: bold">
+                <td>Horse</td>
+                <td>Winning rate by temperature</td>
+                <td>Winning rate by humidity</td>
+                <td>Winning rate by atmospheric pressure</td>
+                <td>Winning rate by luminance</td>
+                <td>Total winning rate</td>
+                </thead>
+
+                <tbody>
+
+                <?php
+                foreach($horseRows as $row) {
+                    echo '<tr>';
+                    echo '<td>' . $row->name . '</td>';
+                    echo '<td>' . $row->temperature . '</td>';
+                    echo '<td>' . $row->humidity . '</td>';
+                    echo '<td>' . $row->pressure . '</td>';
+                    echo '<td>' . $row->temperature . '</td>';
+                    echo '<td>' . $row->totalWinning . '</td>';
+                    echo '</tr>';
+                }
+                ?>
+                <tbody>
+            </table>
+        </div>
+
+    </div>
+    <hr>
+    <footer>
+        <div class="container">
+            <p>&copy; Sami Suo-Heikki & Teemu Kuutti </p>
+        </div>
+    </footer>
+</div> <!-- /container -->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
+
 
