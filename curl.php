fed4a6baec5bcf05df0e8470b62ba894a82169ae
Sami Suo-Heikki <suoheikki.sami@gmail.com>
2015-12-05 12:32:47 +0200

    Most recent data fetch working

--- curl.php
+++ curl.php
@@ -46,11 +46,13 @@ class ThingSee {
     public function getData() {
         $ch = curl_init("http://api.thingsee.com/v2//events/".$this->deviceUuid);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Authorization: Bearer ' . $this->accountAuthToken
         ));
 
-        $result = curl_exec($ch);
-        var_dump($result);
+        $result = json_decode(curl_exec($ch));
+        curl_close($ch);
+        return $result;
     }
 }
--- index.php
+++ index.php
@@ -3,14 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <title>Harness racing</title>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 </head>
 <body>
 <?php
 include 'curl.php';
+include 'weather.php';
+
+$weather = new Weather();
+
 
 $thingsee = new ThingSee();
 $thingsee->getToken();
-$thingsee->getData();
+$currentData = $thingsee->getData();
+$events = $currentData->events;
+foreach($events as $event) {
+    $cause = $event->cause;
+    $senses = $cause->senses;
+    foreach($senses as $sense) {
+        $weather->setValue($sense->sId, $sense->val, $sense->ts);
+    }
+}
+/*var_dump("<pre>");
+var_dump($weather->getHumidity());
+var_dump($weather->getLuminance());
+var_dump($weather->getPressure());
+var_dump($weather->getTemperature());*/
 ?>
 </body>
 </html>
