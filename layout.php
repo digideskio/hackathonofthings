5cba477cda5858a70d02dd85b2ff5a779c8603df
Teemu Kuutti <Teemu Kuutti>
2015-12-05 17:03:24 +0200

    layout improved

--- layout.php
+++ layout.php
@@ -1,198 +1,215 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" href="C:/xampp/favicon.ico">
 
-    <title>Harness Racing APP</title>
+    <title>Harness Racing Wager</title>
 
-    <!-- Bootstrap Core CSS -->
+    <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="css/grayscale.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-    <!-- Custom Fonts -->
+    <!-- Custom styles for this template -->
+    <link href="css/jumbotron.css" rel="stylesheet">
+	
+	<!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+  </head>
 
-</head>
+  <body>
 
-<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
                     <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand page-scroll" href="#page-top">
-                    <i class="fa fa-play-circle"></i> <span class="light">Harness Raging Wager</span>
+		  <a class="navbar-brand page-scroll" href="#">
+                    <i class="fa fa-trophy"></i> <span class="light">Harness Raging Wager</span>
                 </a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
-                <ul class="nav navbar-nav">
-				
-				
-				
-				
-				
-                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#about">Wages</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#download">Profile</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#contact">Settings</a>
-                    </li>
-					
-					<!--
-					<li>
-					<form class="form-inline">
-					  <div class="form-group">
-						<label class="sr-only" for="exampleInputEmail3">Email address</label>
-						<input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
-					  </div>
-					  <div class="form-group">
-						<label class="sr-only" for="exampleInputPassword3">Password</label>
-						<input type="password" class="form-control" id="exampleInputPassword3" placeholder="Password">
-					  </div>
-					  
-					  <button type="submit" class="btn btn-primary">Log in</button>
-					  
-					  <div class="checkbox">
-						<label>
-						  <input type="checkbox"> Remember me
-						</label>
-					  </div>	  
-					  <button type="submit" class="btn btn-success">Sign up</button>
-					</form>
-					</li>
-					-->
-					
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+            <ul class="nav navbar-nav">
+				<!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+				<li class="hidden">
+					<a href="#page-top"></a>
+				</li>
+				<li>
+					<a class="page-scroll" href="#about">Wages</a>
+				</li>
+				<li>
+					<a class="page-scroll" href="#download">Profile</a>
+				</li>
+				<li>
+					<a class="page-scroll" href="#contact">Settings</a>
+				</li>
+            </ul>
+        </div><!--/.navbar-collapse -->
+      </div>
     </nav>
 
-    <!-- Intro Header -->
-    <header class="intro">
-        <div class="intro-body">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
-					
-                   
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Harness Racing Wager!</h1>
+        <p>This site is for waging in harness races</p>
+		
+		<div class="row">
+			<div class="col-md-3">
+			<i class="fa fa-sun-o"></i> luminance
+			</div>
+			<div class="col-md-3">
+			<i class="fa fa-fire"></i> temperature
+			</div>
+			<div class="col-md-3">
+			<i class="fa fa-umbrella"></i> humidity
+			</div>
+			<div class="col-md-3">
+			<i class="fa fa-wrench"></i> pressure
+			</div>
+		</div>
+        <!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+		-->
+      </div>
+    </div>
 
-    <!-- About Section -->
-    <!--
-	<section id="about" class="container content-section text-center">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2">
-                <h2>Harness Racing Wager</h2>
-                <p>Grayscale is a free Bootstrap 3 theme created by Start Bootstrap. It can be yours right now, simply download the template on <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
-                <p>This theme features stock photos by <a href="http://gratisography.com/">Gratisography</a> along with a custom Google Maps skin courtesy of <a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
-                <p>Grayscale includes full HTML, CSS, and custom JavaScript files along with LESS files for easy customization.</p>
-            </div>
-        </div>
-    </section>
-	-->
-
-    <!-- Download Section -->
-	<!--
-    <section id="download" class="content-section text-center">
-        <div class="download-section">
-            <div class="container">
-                <div class="col-lg-8 col-lg-offset-2">
-                    <h2>Download Grayscale</h2>
-                    <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
-                    <a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>
-                </div>
-            </div>
-        </div>
-    </section>
-	-->
 	
-    <!-- Contact Section -->
-	<!--
-    <section id="contact" class="container content-section text-center">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2">
-                <h2>Contact Start Bootstrap</h2>
-                <p>Feel free to email us to provide some feedback on our templates, give us suggestions for new templates and themes, or to just say hello!</p>
-                <p><a href="mailto:feedback@startbootstrap.com">feedback@startbootstrap.com</a>
-                </p>
-                <ul class="list-inline banner-social-buttons">
-                    <li>
-                        <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-                    </li>
-                    <li>
-                        <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </section>
-	-->
+    <div class="container">
+      <!-- Example row of columns -->
+		<div class="row">
+			<div class="col-md-3">
+					<div class="btn-group" data-toggle="buttons">
+						<label class="btn btn-default btn-lg btn-block active">
+							<input type="checkbox" autocomplete="off" checked> EVENT 11
+						</label>
+						<label class="btn btn-default btn-lg btn-block active">
+							<input type="checkbox" autocomplete="off" checked> EVENT 22
+						</label>
+						<label class="btn btn-default btn-lg btn-block active">
+							<input type="checkbox" autocomplete="off" checked> EVENT 33
+						</label>
+						<label class="btn btn-default btn-lg btn-block active">
+							<input type="checkbox" autocomplete="off" checked> EVENT 44
+						</label>
+					</div>
+				</div>
+				
+				<div class="col-md-9">
+				
+					<table class="table table-bordered">
+						<!-- TABLE HEAD -->
+						<thead style="font-weight: bold">
+							<td>Horse Name</td>
+							<td>Temperature</td> 
+							<td>Humidity</td>
+							<td>Atmospheric pressure</td> 
+							<td>Luminance</td>
+							<td>Total Percentage %</td>
+						</thead>
+						
+						<tbody>
+							<!-- 1. ROW-->
+							<tr>
+								<td>Häyhä</td>
+								<td>23</td> 
+								<td>150</td>
+								<td>1005</td> 
+								<td>750</td>
+								<td>X</td>
+							</tr>
+							
+							<!-- 2. ROW-->
+							<tr>
+								<td>Irmeli</td>
+								<td>19</td> 
+								<td>194</td>
+								<td>1005</td> 
+								<td>750</td>
+								<td>X</td>
+							</tr>
+							
+							<!-- 3. ROW-->
+							<tr>
+								<td>Häyhä</td>
+								<td>23</td> 
+								<td>150</td>
+								<td>1005</td> 
+								<td>750</td>
+								<td>X</td>
+							</tr>
+							
+							<!-- 4. ROW-->
+							<tr>
+								<td>Irmeli</td>
+								<td>19</td> 
+								<td>194</td>
+								<td>1005</td> 
+								<td>750</td>
+								<td>X</td>
+							</tr>
+							
+							<!-- 5. ROW-->
+							<tr>
+								<td>Häyhä</td>
+								<td>23</td> 
+								<td>150</td>
+								<td>1005</td> 
+								<td>750</td>
+								<td>X</td>
+							</tr>
+							
+							<!-- 6. ROW-->
+							<tr>
+								<td>Irmeli</td>
+								<td>19</td> 
+								<td>194</td>
+								<td>1005</td> 
+								<td>750</td>
+								<td>X</td>
+							</tr>
+							
+							
+							
+						<tbody>	
+					</table>
+				</div>
 
-    <!-- Map Section -->
-	<!--
-    <div id="map"></div>
-	-->
-	
-    <!-- Footer -->
-    <footer>
-        <div class="container text-center">
-            <p>Copyright &copy; Sami Suo-Heikki & Teemu Kuutti </p>
-        </div>
-    </footer>
+		</div>
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+      <hr>
+	  
+	  
+		<footer>
+			<div class="container">
+				<p>&copy; Sami Suo-Heikki & Teemu Kuutti </p>
+			</div>
+		</footer>
+    </div> <!-- /container -->
 
-    <!-- Bootstrap Core JavaScript -->
+   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="js/jquery.easing.min.js"></script>
-
-    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="js/grayscale.js"></script>
-
-</body>
-
+  </body>
 </html>
